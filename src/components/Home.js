6c05b8234d7052b9ab36d619c893b8884238162e
Adida <adida.barack@student.moringaschool.com>
2024-04-27 10:49:29 +0300

    Done Lab

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,11 @@
 import React from "react";
-
-function Home() {
+import { username } from "../data/user";
+import { city } from "../data/user";
+export default function Home() {
   return (
     <div id="home">
+      <city />
+      <username />
       <h1>
         {username} is a Web Developer from {city}
       </h1>
